271effb392eadc76a69ce8f9520d23c646a40404
Nilay D. Shenai <70856655+NilayShenai@users.noreply.github.com>
2026-06-02 09:20:00 +0530

    feat: define core synthesis types and structures

---
+++ types.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Type definitions aggregate module.
+ */
+
+export * from './types/enums';
+export * from './types/targets';
+export * from './types/params';
+export * from './types/state';
+export * from './types/midi';
+export * from './types/audio';
+export * from './types/persistence';
